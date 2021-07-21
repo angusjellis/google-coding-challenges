2fb82ab6e9891a6225bb1316ecd25ef309e1f1ab
angusjellis@live.com <angusjellis@live.com>
2021-07-21 23:39:33 +0100

    pretty sure something is wrong with line 35

--- c1.py
+++ c1.py
@@ -23,23 +23,27 @@ def find_largest_tuple(s):
                 candidates = set(valid_strings.values())
                 # print(candidates)
                 test_cans = [x for x in candidates]
+                # print(test_cans)
+                valid_cans = {}
+                fits_end = []
                 for c in test_cans:
-                    pos = len(c)
-                    
-                    end = s[-pos:]
-                    if c != end:
-                        print("uh oh")
+                    pos = (len(c)+1)
+                    end = s[:-pos]
+                    if c == end:
+                        fits_end.append(c)
                         print(c)
-                        print(end)
-                    else:
-                        print("good")
-                        print(c)
-                        print(end)
+                for start in range(0,len(fits_end)-length+1):
+                        valid_cans[start] = tuple(fits_end[start:start+length])
+                print(valid_cans)
+                final_set = set(valid_cans.values())
+                # print(valid_cans)
                 if len(candidates) != len(valid_strings):
-                        storage = valid_strings
+                    storage = valid_strings
                 else:
                         break
+        
         first_sol = storage[0]
+        print(first_sol)
         return first_sol
 
 
